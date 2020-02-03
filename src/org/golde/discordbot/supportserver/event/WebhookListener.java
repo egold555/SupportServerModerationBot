63142d239390902304e473ca76b0c8535577b9e9
Eric Golde <eric@golde.org>
2020-02-02 16:30:10 -0800

    remove swearing

--- src/org/golde/discordbot/supportserver/event/WebhookListener.java
+++ src/org/golde/discordbot/supportserver/event/WebhookListener.java
@@ -1,5 +1,7 @@
 package org.golde.discordbot.supportserver.event;
 
+import org.golde.discordbot.supportserver.util.Channels;
+
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
@@ -37,7 +39,7 @@ public class WebhookListener extends AbstractMessageChecker {
 			String link = parts[1];
 			
 			if(true/*title.toLowerCase().startsWith("How to code a Minecraft PVP Client".toLowerCase())*/) {
-				guild.getTextChannelById("667231090465046539").sendMessage("<a:RedAlert:665412686175010816> **Hey " + guild.getRoleById("667224721544183838").getAsMention() + ", Eric has uploaded a new video**: " + title + "\n\nWatch it here: " + link).queue();
+				guild.getTextChannelById(Channels.ANNOUNCEMENTS).sendMessage("<a:RedAlert:665412686175010816> **Hey " + guild.getRoleById("667224721544183838").getAsMention() + ", Eric has uploaded a new video**: " + title + "\n\nWatch it here: " + link).queue();
 			}
 			
 			
