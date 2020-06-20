f310c76c9b9ee2857ff4dd7bf2fb5ffd173eadcf
Daan Breur <daanbreur@gmail.com>
2020-06-20 09:54:50 +0200

    [Rich Embed] Added Rich Embed to the Ping Command

--- src/org/golde/discordbot/supportserver/command/everyone/CommandPing.java
+++ src/org/golde/discordbot/supportserver/command/everyone/CommandPing.java
@@ -4,8 +4,12 @@ import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 import com.jagrosh.jdautilities.command.CommandEvent;
+import net.dv8tion.jda.api.EmbedBuilder;
 
 public class CommandPing extends EveryoneCommand {
+
+	private long ping = 0;
+	private long websocket = 0;
 
 	public CommandPing() {
 		super("ping", null, "checks the bot's latency", "pong");
@@ -14,10 +18,22 @@ public class CommandPing extends EveryoneCommand {
 	
 	@Override
 	protected void execute(CommandEvent event, List<String> args) {
-		event.reply("Ping: ...", m -> {
-            long ping = event.getMessage().getTimeCreated().until(m.getTimeCreated(), ChronoUnit.MILLIS);
-            m.editMessage("Ping: " + ping  + "ms | Websocket: " + event.getJDA().getGatewayPing() + "ms").queue();
-        });
+
+		event.reply("Pinging......", m -> {
+			ping = event.getMessage().getTimeCreated().until(m.getTimeCreated(), ChronoUnit.MILLIS);
+			websocket = event.getJDA().getGatewayPing();
+
+			m.delete();
+		});
+
+		EmbedBuilder builder = new EmbedBuilder()
+				.setTitle("Network Information")
+				.addField("Ping: ", ping + " ms", false)
+				.addField("Websocket: ", websocket + " ms", false)
+				;
+
+		event.getChannel().sendMessage(builder.build()).queue();
+
 	}
 
 }
