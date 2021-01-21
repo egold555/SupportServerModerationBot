d693ab498aa4d06f1a22b0fdadf6607945c79ed6
Eric Golde <eric@golde.org>
2021-01-20 21:59:52 -0800

    Remove version shit idea

--- Website/src/org/golde/discordbot/website/server/routes/PageIndex.java
+++ Website/src/org/golde/discordbot/website/server/routes/PageIndex.java
@@ -3,7 +3,6 @@ package org.golde.discordbot.website.server.routes;
 import java.util.Map;
 
 import org.golde.discordbot.shared.ESSBot;
-import org.golde.discordbot.shared.util.VersionUtil;
 import org.golde.discordbot.website.server.ESSWebServer;
 import org.golde.discordbot.website.server.routes.base.AbstractJsonResponse;
 
@@ -15,7 +14,6 @@ public class PageIndex extends AbstractJsonResponse {
 
 	@Override
 	public JsonObject getResponse(Map<String, String> urlParams, IHTTPSession session, JsonObject root) {
-		root.addProperty("git", VersionUtil.getHash());
 		root.addProperty("uptime", System.currentTimeMillis() - ESSWebServer.start);
 		root.add("routes", ESSBot.GSON.toJsonTree(ESSWebServer.routes));
 		return root;
