6a33deb788537fdbdf57cbee52b28d0b372bf173
Eric Golde <eric@golde.org>
2021-01-20 21:32:56 -0800

    added git version to the main page

--- Website/src/org/golde/discordbot/website/server/routes/PageIndex.java
+++ Website/src/org/golde/discordbot/website/server/routes/PageIndex.java
@@ -3,6 +3,7 @@ package org.golde.discordbot.website.server.routes;
 import java.util.Map;
 
 import org.golde.discordbot.shared.ESSBot;
+import org.golde.discordbot.shared.util.VersionUtil;
 import org.golde.discordbot.website.server.ESSWebServer;
 import org.golde.discordbot.website.server.routes.base.AbstractJsonResponse;
 
@@ -14,6 +15,7 @@ public class PageIndex extends AbstractJsonResponse {
 
 	@Override
 	public JsonObject getResponse(Map<String, String> urlParams, IHTTPSession session, JsonObject root) {
+		root.addProperty("git", VersionUtil.getHashShort());
 		root.addProperty("uptime", System.currentTimeMillis() - ESSWebServer.start);
 		root.add("routes", ESSBot.GSON.toJsonTree(ESSWebServer.routes));
 		return root;
