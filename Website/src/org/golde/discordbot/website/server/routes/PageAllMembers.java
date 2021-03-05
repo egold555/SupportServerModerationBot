cbdc4977768a12ef654079a20897084972f8b264
Eric Golde <eric@golde.org>
2021-03-04 19:17:23 -0800

    Alphabetical sorting on the api

--- Website/src/org/golde/discordbot/website/server/routes/PageAllMembers.java
+++ Website/src/org/golde/discordbot/website/server/routes/PageAllMembers.java
@@ -2,6 +2,7 @@ package org.golde.discordbot.website.server.routes;
 
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
@@ -28,23 +29,23 @@ public class PageAllMembers extends AbstractJsonResponse {
 			setErrored("Only GET is supported", Status.BAD_REQUEST);
 			return root;
 		}
-		
+
 		Guild g = WebsiteBot.getInstance().getGuild();
 
 		List<JsonObject> objRoleList = new ArrayList<JsonObject>();
-		
+
 
 		for(int i = 0; i < g.getRoleCache().size(); i++) {
 
 			Role role = g.getRoleCache().asList().get(i);
-			
+
 			if(role.getIdLong() == Roles.EVERYONE || 
-					role.getIdLong() == Roles.INTERNAL_DISCORD_BETA_TESTER || 
-//					role.getName().contains("Bot") ||
+					role.getIdLong() == Roles.SUSPICIOUS_ACCOUNT_REPORTER || 
+					//					role.getName().contains("Bot") ||
 					role.getIdLong() == Roles.ITS_MY_B_DAY || 
 					role.getIdLong() == Roles.YOUTUBE_NOTIFICATIONS ||
 					role.getIdLong() == Roles.MUTED ||
-//					role.getIdLong() == Roles.MEMBER ||
+					//					role.getIdLong() == Roles.MEMBER ||
 					role.getName().contains("Internal - ") || 
 					role.getName().contains("Notifications") || 
 					role.getName().equals("Server Captcha Bot")
@@ -52,18 +53,30 @@ public class PageAllMembers extends AbstractJsonResponse {
 				continue;
 			}
 
+			List<JsonObject> membersInRole = getMembers(g, role);
+			
+			if(membersInRole.isEmpty()) {
+				continue;
+			}
+			
 			JsonObject roleObj = new JsonObject();
 			roleObj.addProperty("name", role.getName());
 
 			Color roleColor = role.getColor();
-			if(role.getColor() == null) {
+
+			if(role.getIdLong() != Roles.MEMBER && (role.getColor() == null || role.getColorRaw() == Role.DEFAULT_COLOR_RAW)) {
+				continue;
+			}
+
+			if(roleColor == null) {
 				roleColor = new Color(Role.DEFAULT_COLOR_RAW);
 			}
-			
+
+
 			String hex = "#"+Integer.toHexString(roleColor.getRGB()).substring(2).toUpperCase();
 			roleObj.addProperty("color", hex);
 
-			roleObj.add("members", ESSBot.GSON.toJsonTree(getMembers(g, role)));
+			roleObj.add("members", ESSBot.GSON.toJsonTree(membersInRole));
 
 			objRoleList.add(roleObj);
 
@@ -77,12 +90,23 @@ public class PageAllMembers extends AbstractJsonResponse {
 	List<Long> duplateMembers = new ArrayList<Long>();
 	private List<JsonObject> getMembers(Guild g, Role role) {
 		List<JsonObject> list = new ArrayList<JsonObject>();
-		for(Member m : g.getMembersWithRoles(role)) {
+		List<Member> sorted = g.getMembersWithRoles(role);
+		
+		sorted.sort(new Comparator<Member>() {
+
+			@Override
+			public int compare(Member o1, Member o2) {
+				return o1.getEffectiveName().compareTo(o2.getEffectiveName());
+			}
+			
+		});
+		
+		for(Member m : sorted) {
 			if(!duplateMembers.contains(m.getIdLong())) {
 				list.add(toJson(m));
 				duplateMembers.add(m.getIdLong());
 			}
-			
+
 		}
 		return list;
 	}
@@ -97,5 +121,5 @@ public class PageAllMembers extends AbstractJsonResponse {
 
 		return obj;
 	}
-	
+
 }
