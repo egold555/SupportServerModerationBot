84ac6f17aeb9a2874954b915eeff7151921f4ad6
Eric Golde <eric@golde.org>
2021-03-15 19:59:45 -0700

    Remove website for the time being until the rewrite is done

--- Website/src/express/ExpressListener.java
+++
@@ -1,9 +0,0 @@
-package express;
-
-/**
- * @author Simon Reinisch
- * Listener for express actions
- */
-public interface ExpressListener {
-    void action();
-}
--- Website/src/express/http/SameSite.java
+++
@@ -1,9 +0,0 @@
-package express.http;
-
-/**
- * @author Simon Reinisch
- */
-public enum SameSite {
-    STRICT,
-    LAX
-}
